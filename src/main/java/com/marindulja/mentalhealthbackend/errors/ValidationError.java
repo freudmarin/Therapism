44d83d6ec515c2ea373dff84c62c66bc0dd3e4ca
Marin Dulja <freudmarin@gmail.com>
2024-06-15 16:28:53 +0200

    Handled more exceptions globally Added validation for signup/login

---
+++ src/main/java/com/marindulja/mentalhealthbackend/errors/ValidationError.java
@@ -0,0 +1,13 @@
+package com.marindulja.mentalhealthbackend.errors;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class ValidationError {
+    private String code;
+    private String message;
+}
---
+++ src/main/java/com/marindulja/mentalhealthbackend/responses/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.marindulja.mentalhealthbackend.responses;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.marindulja.mentalhealthbackend.errors.ValidationError;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+public class ErrorResponse {
+    private Integer statusCode;
+    private HttpStatus status;
+    private String reason;
+    private String message;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Dhaka")
+    private Date timestamp;
+
+    private Map<String, List<ValidationError>> validationErrors;
+}
